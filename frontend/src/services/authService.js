b5eab0a43ba4948dce175cdb1343b4ec8df952a4
dakshbhuva-78 <dakshbhuva78@gmail.com>
2026-06-29 17:10:22 +0530

    Add Environment Variable File to Frontend

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/users";
+const API_URL = `${import.meta.env.VITE_API_URL}/users`;
 
 export const loginUser = async (userData) => {
   const response = await axios.post(
@@ -12,23 +12,40 @@ export const loginUser = async (userData) => {
 };
 
 export const registerUser = async (userData) => {
-  const response = await axios.post(
+
+  const { data } = await axios.post(
     `${API_URL}/register`,
     userData
+  );
+
+  return data;
+};
+
+export const getUserProfile = async (token) => {
+
+  const response = await axios.get(
+    `${API_URL}/profile`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
   );
 
   return response.data;
 };
 
-export const getUserProfile = async (token) => {
-  const response = await axios.get(
+export const updateUserProfile = async (userData, token) => {
+
+  const { data } = await axios.put(
     `${API_URL}/profile`,
+    userData,
     {
       headers: {
-        Authorization: `Bearer ${token}`
-      }
+        Authorization: `Bearer ${token}`,
+      },
     }
   );
 
-  return response.data;
+  return data;
 };
